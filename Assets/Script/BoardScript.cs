130d0210e4f4b8ddffeaa778b140d43b12e9c359
tomato323 <105186437+tomato323@users.noreply.github.com>
2022-06-13 22:11:46 +0900

    更新

--- Assets/Script/BoardScript.cs
+++ Assets/Script/BoardScript.cs
@@ -21,8 +21,11 @@ public class BoardScript : MonoBehaviour
 
     Vector2 pos;
 
-    
-   
+    private void Start()
+    {
+        CreateBoard();
+    }
+
 
     void CreateBoard()
     {
@@ -47,18 +50,18 @@ public class BoardScript : MonoBehaviour
         {
             pos = RoundingScript.Round(item.position);
 
-        }
 
-        if (!BoardOutCheck((int)pos.x, (int)pos.y))
-        {
-            return false;
-        }
 
-        if (BlockCheck((int)pos.x, (int)pos.y,block))
-        {
-            return false;
-        }
+            if (!BoardOutCheck((int)pos.x, (int)pos.y))
+            {
+                return false;
+            }
 
+            if (BlockCheck((int)pos.x, (int)pos.y, block))
+            {
+                return false;
+            }
+        }
         return true;    
     }
 
@@ -82,9 +85,78 @@ public class BoardScript : MonoBehaviour
     {
         foreach (Transform item in block.transform)
         {
-            Vector2 pos = RoundingScript.Round(item.position);
+            pos = RoundingScript.Round(item.position);
 
             grid[(int)pos.x, (int)pos.y] = item;
         }
+    }
+
+    public void ClearAllRows()
+    {
+        for (int y = 0; y < height; y++)
+        {
+            if (IsComplete(y))
+            {
+                ClearRow(y);
+
+                ShiftRowsDown(y + 1);
+
+                y--;
+            }
+        }
+    }
+
+    bool IsComplete(int y)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            if (grid[x,y] == null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void ClearRow(int y)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            if (grid[x, y] != null)
+            {
+                Destroy(grid[x, y].gameObject);
+                Debug.Log("block");
+            }
+            grid[x,y] = null;
+        }
+    }
+
+    void ShiftRowsDown(int startY)
+    {
+        for (int y = startY; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                if (grid[x,y] != null)
+                {
+                    grid[x, y - 1] = grid[x, y];
+                    grid[x,y] = null;
+                    grid[x, y - 1].position += new Vector3(0, -1, 0);
+                }
+            }
+        }
+    }
+
+   public bool OverLimit(BlockScript block)
+    {
+        foreach(Transform item in block.transform)
+        {
+            if (item.transform.position.y >= height -header)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
--- Assets/Script/GameManagerScript.cs
+++ Assets/Script/GameManagerScript.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManagerScript : MonoBehaviour
 {
@@ -15,10 +17,24 @@ public class GameManagerScript : MonoBehaviour
 
     BoardScript board;
 
+    float nextKeyDownTimer, nextKeyLeftRightTimer,nextKeyRotateTimer;
 
+    public Text lastScoreText;
+    int lastScore;
 
+    public Text scoreText;
+    private int score;
+
+  
+    [SerializeField]
+    private float nextKeyDownInterval, nextKeyLeftRightInterval,nextKeyRotateInterval;
 
     //スポナーオブジェクトをスポナー変数に格納するコードの記述
+
+    [SerializeField]
+    private GameObject gameOverPanel;
+
+    bool gameOver;
 
     [SerializeField]
     private float dropInterval = 0.25f;
@@ -33,6 +49,13 @@ public class GameManagerScript : MonoBehaviour
 
         spawner.transform.position = RoundingScript.Round(spawner.transform.position);
 
+        nextKeyDownTimer = Time.time + nextKeyDownInterval;
+        nextKeyLeftRightTimer = Time.time + nextKeyLeftRightInterval;
+        nextKeyRotateTimer = Time.time + nextKeyRotateInterval;
+
+
+
+
 
         if (!activeBlock)
         {
@@ -40,39 +63,150 @@ public class GameManagerScript : MonoBehaviour
             activeBlock = spawner.SpawnBlock();
         }
 
+        if (gameOverPanel.activeInHierarchy)
+        {
+            gameOverPanel.SetActive(false);
+        }
        
     }
 
     private void Update()
     {
-        if (Time.time > nextdropTimer)
+
+        if (gameOver)
         {
-            nextdropTimer = Time.time + dropInterval;
+            return;
+        }
 
-            if (activeBlock)
+        PlayerInput();
+    }
+
+
+    void PlayerInput()
+    {
+        if (Input.GetKey(KeyCode.D) && (Time.time > nextKeyLeftRightTimer)||Input.GetKeyDown(KeyCode.D))
+        {
+            activeBlock.MoveRight();
+
+            nextKeyLeftRightTimer = Time.time + nextKeyLeftRightInterval;
+
+            if (!board.CheckPosition(activeBlock))
             {
-
-                activeBlock.MoveDown();
-
-
-                if (!board.CheckPosition(activeBlock))
-                {
-                    activeBlock.MoveUp();
-
-                    board.SaveBlockInGrid(activeBlock);
-
-                    activeBlock = spawner.SpawnBlock(); 
-                }
+                activeBlock.MoveLeft();
             }
         }
 
-
+        else if (Input.GetKey(KeyCode.A) && (Time.time > nextKeyLeftRightTimer) || Input.GetKeyDown(KeyCode.A))
         
+        {
+            activeBlock.MoveLeft();
+
+            nextKeyLeftRightTimer = Time.time + nextKeyLeftRightInterval;
+
+            if (!board.CheckPosition(activeBlock))
+            {
+                activeBlock.MoveRight();
+            }
+        }
+
+        else if (Input.GetKey(KeyCode.E) && (Time.time >nextKeyRotateTimer))
+        {
+            activeBlock.RotateRight();
+            nextKeyRotateTimer = Time.time + nextKeyRotateInterval;
+
+            if (!board.CheckPosition(activeBlock))
+            {
+                activeBlock.RotateLeft();
+            }
+
+        }
+        else if (Input.GetKey(KeyCode.S) && (Time.time > nextKeyDownTimer) || (Time.time > nextdropTimer))
+
+        {
+            activeBlock.MoveDown();
+
+            nextKeyLeftRightTimer = Time.time + nextKeyDownInterval;
+            nextdropTimer = Time.time + dropInterval;
+
+            if (!board.CheckPosition(activeBlock))
+            {
+                if (board.OverLimit(activeBlock))
+                {
+                    GameOver();
+                }
+                else
+                {
+
+                    BottomBoard();
+                }
+            }
+        }
+    }
+
+    void BottomBoard()
+    {
+        activeBlock.MoveUp();
+        board.SaveBlockInGrid(activeBlock);
+
+        activeBlock = spawner.SpawnBlock();
+
+        nextKeyDownTimer = Time.time;
+        nextKeyLeftRightTimer = Time.time;
+        nextKeyRotateTimer = Time.time;
+
+        board.ClearAllRows();
+
     }
 
     //スポナークラスからブロック生成関数を呼んで変数に格納する
 
 
     // Update is called once per frame
+
+    void GameOver()
+    {
+        activeBlock.MoveUp();
+
+        if (!gameOverPanel.activeInHierarchy)
+        {
+            gameOverPanel.SetActive(true);
+        }
+
+        gameOver = true;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+
+    private int AddScore(int rowNum)
+    {
+        int addScore;
+
+        if(rowNum == 1)
+        {
+            addScore = 10;
+        }
+        else if(rowNum == 2)
+        {
+            addScore = 20;
+        }
+        else if(rowNum==3)
+        {
+            addScore = 50;
+        }
+        else if(rowNum ==4)
+        {
+            addScore = 100;
+        }
+        else
+        {
+            addScore = 0;
+        }
+        return addScore;
+    }
+
+
 
 }
